ddd272f4c41abec8dd1265f754bcbe0d4446e0c5
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-28 10:46:31 -0700

    Invoke action creators for showing todos via onClick

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import AddTodoForm from '../containers/AddTodoForm';
 import ToDoList from '../containers/ToDoList';
+import { showAll, showActive, showCompleted } from '../actions/index'
+import { connect } from 'react-redux';
 
 class App extends Component {
+  //do I need to destructure props for 
+  //showAll, showActive, and showComplete here?
+  
   render() {
     return (
       <div className="App">
         <h1>Another Todo Box</h1>
+        <button onClick={() => showAll()}>Show All</button>
+        <button onClick={() => showActive()}>Show Active</button>
+        <button onClick={() => showCompleted()}>Show Completed</button>
         <AddTodoForm />
         <ToDoList />
       </div>
@@ -14,4 +22,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  showAll: () => dispatch( showAll() ),
+  showActive: () => dispatch( showActive() ),
+  showCompleted: () => dispatch( showCompleted() )
+})
+
+export default connect(null, mapDispatchToProps)(App);
