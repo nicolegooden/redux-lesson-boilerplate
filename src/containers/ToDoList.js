cf9d02e99b4e2c267084280460f3f3a5bbfc4864
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-27 21:57:10 -0700

    Add variable to display mapped todo cards

---
+++ src/containers/ToDoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { TodoCard } from '../components/TodoCard'
+
+const ToDoList = ({todos}) => {
+  const allTodos = todos.map(todo => {
+    return (
+      <TodoCard 
+        id={todo.id}
+        todo={todo.todo}
+        completed={todo.completed}
+      />
+    )
+  })
+  return (
+    <section>{allTodos}</section>
+  )
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+export default connect(mapStateToProps)(ToDoList);
