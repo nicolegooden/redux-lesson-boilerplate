3fe5d1aba86333518676b8f1c482b2abadd33c08
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-28 10:10:49 -0700

    Update case for complete todo

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -3,7 +3,12 @@ export const todos = (state = [], action) => {
     case 'ADD_TODO':
       return [...state, { id: Date.now(), todo: action.todo, completed: false }];
     case 'COMPLETE_TODO':
-      return [...state, {id: action.id, todo: action.todo, completed: true}];
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
     default:
       return state;
   }
