b4d5193b2a677c2990f5fe381e49b3cd006be175
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-28 10:27:16 -0700

    Remove unused showCompleteToDo from App

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,12 @@ import AddTodoForm from '../containers/AddTodoForm';
 import ToDoList from '../containers/ToDoList';
 
 class App extends Component {
-
-  showCompleteToDo = (id, todo) => {
-    //map state to props at bottom of this file
-    //FIND the todo that was clicked on by user (of all todos)
-    //trigger the completeToDo action
-    //add the class "completed" to that todo's JSX in TodoCard
-  }
-
   render() {
     return (
       <div className="App">
         <h1>Another Todo Box</h1>
         <AddTodoForm />
-        <ToDoList showCompleteToDo={this.showCompleteToDo}/>
+        <ToDoList />
       </div>
     );
   }
